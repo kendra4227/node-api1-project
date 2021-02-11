7e301473bddd90f19c39d0cfbc58ccd679c097e0
Lakendria <kendra939@gmail.com>
2021-02-10 22:05:22 -0500

    initial setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,39 @@
 // BUILD YOUR SERVER HERE
+const { json } = require("express")
+const express = require("express")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+const db=require("./users/model")
+server.get("/",(req,res)=>{
+    res.json({message:"server is running"})
+})
+
+//Create user 
+server.post("/api/users",(req,res)=>{
+    const userData = req.body
+
+
+    
+})
+
+//Return an array of users
+server.get("/api/users",(req,res)=>{
+    const users= db.find()
+    res.json(users)
+    
+})
+//Returns the user object with the specified id
+server.get("/api/users/:id",(req,res)=>{
+
+})
+// Removes the user with the specified id and return the delete user
+server.delete("/api/users/:id",(req,res)=>{
+
+})
+//Updates the user with the specified id
+server.put("/api/users/:id",(req,res)=>{
+
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
