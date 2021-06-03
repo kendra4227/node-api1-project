1cf873093320ca8dab10d82534a8a8490ba27dde
Lakendria <kendra939@gmail.com>
2021-06-02 21:18:15 -0400

    created new user

--- api/server.js
+++ api/server.js
@@ -39,7 +39,27 @@ server.get("/api/users/:id",(req,res)=>{
 })
 //| POST   | /api/users     | Creates a user using the information sent inside the `request body`. 
 
-server.post()
+server.post("/api/users",(req,res)=>{
+    const newUser = req.body
+    if(!newUser.name || !newUser.bio){
+        res.status(404).json( "Please provide name and bio for the user")
+    }
+    else{
+    User.insert(newUser)
+    .then(users=>{
+        res.status(201).json(users)
+    })
+    .catch(err=>{
+        res.status(500).json({message:err.message})
+    })
+    }
+})
+
+//| DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.  
+
+
+
+//| PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
 
 
 
